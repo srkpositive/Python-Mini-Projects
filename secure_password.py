2211229553a14c9dca8c346905f36d380f0b5803
Shah Rukh <83986319+srkpositive@users.noreply.github.com>
2022-02-15 20:35:55 +0530

    Securing your current password in Python

---
+++ secure_password.py
@@ -0,0 +1,24 @@
+'''
+Secure an existing password by replacing a set of characters
+with the corresponding 'password-secure' character (provided as tuple)
+Example:
+    SECURE = (('s', '$'), ('and', '&'), ('a', '@')
+             ('o', '0'), ('i', '1'), ('I', '|'))
+    Input:
+    password = "Indians123"
+    
+    Output:
+    Your secure password is: |nd1@n$123
+'''
+
+SECURE = (('s', '$'), ('and', '&'), ('a', '@'), ('o', '0'), ('i', '1'), ('I', '|'))
+def securePassword(password):
+    for a,b in SECURE:
+        password = password.replace(a, b)
+    return password
+
+if __name__ == "__main__":
+    while(True):
+        password = input("Enter your password: ")
+        password = securePassword(password)
+        print(f"Your secure password is {password}")
